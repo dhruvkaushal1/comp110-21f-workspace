f0e2babdf5003639293d2eabdc608286a417a531
Dhruv Kaushal <dhruvk@unc.edu>
2021-09-15 23:09:59 -0400

    premptive submission ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,19 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730484925"
+word: str = input("Enter a word: ")
+i: int = 0
+j: int = i + 1
+letter: str 
+duplicate: str
+same: bool = False
+while i < len(word):
+    letter = word[i]
+    j = i + 1
+    while j < len(word):
+       duplicate = word[j]
+       j += 1
+       if (duplicate == letter):
+           same = True
+    i += 1
+print("Found duplicate: " + str(same))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,3 +4,10 @@ __author__ = "123456789"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+depth: int = int(input("Depth: "))
+i: int = 0
+print_tree: str = ""
+while i < depth:
+    print_tree = print_tree + TREE 
+    print(print_tree)
+    i = i + 1
