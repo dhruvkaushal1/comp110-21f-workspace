7f3e275b6ba8164417d54545436e81eef203319f
Dhruv Kaushal <dhruvk@unc.edu>
2021-09-24 22:04:28 -0400

    DONE2

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -38,7 +38,7 @@ def greet() -> None:
     print(f"Hello, {player}, you are now going to embark on a \njourney where you will battle tyrants of our world")
     print("Now, you will be bestowed a creature from our world!\n")
     time.sleep(7)
-    cow_number: int = randint(1,3)
+    cow_number: int = randint(1, 3)
     if cow_number == 1:
         global cow_type
         global cow_health
@@ -55,7 +55,7 @@ def greet() -> None:
     
 
 def easy_boss() -> None:
-    """"Easy boss is a simple attack based boss which is easy to beat."""
+    """Easy boss is a simple attack based boss which is easy to beat."""
     print("Your first boss is the nefarious Electric Giraffe!!! \nThe Electric Giraffe has taken a hold over our land's electrical grid and takes away all our power \nWe need your help to take him down!")
     electirc_giraffe_health: int = 100
     print("Your Battle has begun. Enter the attacks and defenses that you would like to use when asked")
@@ -65,8 +65,8 @@ def easy_boss() -> None:
         electirc_giraffe_health = electirc_giraffe_health - 40
         print(f"Girraffe Health: {electirc_giraffe_health}")
     else:
-       electirc_giraffe_health = electirc_giraffe_health - 25
-       print(f"Girraffe Health: {electirc_giraffe_health}")
+        electirc_giraffe_health = electirc_giraffe_health - 25
+        print(f"Girraffe Health: {electirc_giraffe_health}")
     defense_one: str = input("Would you like to use time shield, or light shield? ") 
     if defense_one == "time shield":
         print(f"{player}, giraffes are clearly impervious to time control in our realm \n Electric Giraffe's attack was not blocked!!")
@@ -76,7 +76,7 @@ def easy_boss() -> None:
     else:
         print("Good choice, Electric Giraffe's attack was blocked!!")
     attack_two: str = input("Would you like to use super slap, or milk attack? ")
-    if attack_one == "super slap":
+    if attack_two == "super slap":
         electirc_giraffe_health = electirc_giraffe_health - 80
         print(f"Girraffe Health: {electirc_giraffe_health}")
     else: 
@@ -90,53 +90,47 @@ def easy_boss() -> None:
 
 
 def hard_boss(hardhealth: int) -> int:
-    """"Hard boss takes a longer time than easy boss and incorporates a random variable within defense."""
+    """Hard boss takes a longer time than easy boss and incorporates a random variable within defense."""
     global player
     print("Your boss is the villinaous fire rhino!!! \n The Fire Rhino has been the tyrant of our land for so long. \nHe is the boss of all of the crminals")
     print(f"Taking him on is not going to be an easy challenge, but, {player}, You can do it!!!")
     print("Your Battle has begun. Enter the attacks and defenses that you would like to use when asked")
     print(cow_emoji)
     fire_rhino_health: int = 150
-    #global cow_health
     global cow_type
     attack_one: str = input("Would you like to use brave cow or cow beam? ")
     if attack_one == "brave cow":
         fire_rhino_health = fire_rhino_health - 20
         print(f"Fire Rhino Health: {fire_rhino_health}")
     else:
-       fire_rhino_health = fire_rhino_health - 15
-       print(f"Fire Rhino Health: {fire_rhino_health}")
+        fire_rhino_health = fire_rhino_health - 15
+        print(f"Fire Rhino Health: {fire_rhino_health}")
     defense_one: str = input("Would you like to use milk shield, or grass defense? ") 
     if defense_one == "milk shield":
         print("Fire Rhino's core temperature evaporated all of the liquid!! \nFire Rhino's attack was not blocked")
-        #global cow_health
-        #cow_health = cow_health - 20
         hardhealth = hardhealth - 20
         print(f"Cow Health: {hardhealth}")
     else:
         print(f"The Grass was set ablaze by Fire Rhino's fire attack!! \nThis caused extra burn on your, {cow_type}")
-        #cow_health = cow_health - 40
         hardhealth = hardhealth - 40
         print(f"Cow Health: {hardhealth}")
     attack_two: str = input("Would you like to use cow tail or outrage? ")
-    if attack_one == "cow tail":
+    if attack_two == "cow tail":
         fire_rhino_health = fire_rhino_health - 25
         print(f"Fire Rhino Health: {fire_rhino_health}")
     else: 
         fire_rhino_health = fire_rhino_health - 35
         print(f"Fire Rhino Health: {fire_rhino_health}")
     defense_two: str = input("Would you like to use cotton guard or ice shield? ") 
-    critical_hit: int = randint(1,6)
+    critical_hit: int = randint(1, 6)
     if critical_hit < 3:
         print(f"No Good, {player}, your defense failed")
-        #cow_health = cow_health - 40
         hardhealth = hardhealth - 40
     elif defense_two == "ice shield":
         print("Good Choice!! The ice Nullifed Fire Rhino's attack \nFire Rhino's attack was blocked")
         print(f"Cow Health: {hardhealth}")
     else:
         print(f"The Cotton was set ablaze by Fire Rhino's fire attack!! \nThis caused extra burn on your, {cow_type}")
-        #cow_health = cow_health - 40
         hardhealth = hardhealth - 40
         print(f"Cow Health: {hardhealth}")
     print("\n\n *A bright shining light appears in the sky. It calls to you.\nYou let it come to you and open your hand naturally\n In a blinding light, you are left with a stone in your hand*")
@@ -146,13 +140,12 @@ def hard_boss(hardhealth: int) -> int:
     if attack_three == "multi shadow cow":
         hardhealth = hardhealth - 25
         print("Recoil from the attack lowered your cow's health")
-        print(f"Fire Rhino Health: 0")
+        print("Fire Rhino Health: 0")
     else:
         hardhealth = hardhealth - 20
         print("Recoil from the attack lowered your cow's health")
     print(f"{player} you have defeated the horrible Fire Rhino")
     print(f"Your score for this round is {hardhealth}")
-    print(cow_emoji)
     return hardhealth
 
 
